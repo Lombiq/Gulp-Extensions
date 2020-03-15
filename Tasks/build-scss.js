fb8a1f379a1740929c696ee881a9ce06b663592c
Márk Bartha <mark.bartha@lombiq.com>
2020-03-15 11:56:34 +0100

    Adding postcss with autoprefixer to build-scss

--- Tasks/build-scss.js
+++ Tasks/build-scss.js
@@ -7,14 +7,21 @@ const sass = require('gulp-sass');
 const nodeSass = require('node-sass');
 const rename = require('gulp-rename');
 const cleanCss = require('gulp-clean-css');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 sass.compiler = nodeSass;
 
-const buildScss = function (source, destination) {
+const defaultCompatibleBrowsers = ["last 1 Chrome version", "last 1 Edge version", "last 1 Firefox version", "last 1 IE version", "last 1 iOS version"];
+
+const buildScss = function (source, destination, compatibleBrowsers) {
+    compatibleBrowsers = compatibleBrowsers ? compatibleBrowsers : defaultCompatibleBrowsers;
+
     return gulp.src(source)
         .pipe(cache('scss'))
         .pipe(plumber())
         .pipe(sass({ linefeed: 'crlf' })).on('error', sass.logError)
+        .pipe(postcss([autoprefixer({ overrideBrowserslist: compatibleBrowsers })]))
         .pipe(gulp.dest(destination))
         .pipe(cleanCss({ compatibility: 'ie8' }))
         .pipe(rename({ extname: '.min.css' }))
