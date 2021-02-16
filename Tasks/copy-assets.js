c54cbb48b367ae96600c60cd9204e7427ca377ee
Márk Lukovics <mark.lukovics@lombiq.com>
2021-02-16 13:25:17 +0100

    using ESLint on the copy-assets gulp command file

--- Tasks/copy-assets.js
+++ Tasks/copy-assets.js
@@ -4,10 +4,9 @@ const gulp = require('gulp');
 const all = require('gulp-all');
 
 const copyAssets = function (assets, destination, beforeDest) {
-    const src = asset => gulp.src(asset.path);
-    return all(assets.map((asset) =>
-        (beforeDest ? beforeDest(src(asset), asset) : src(asset))
-            .pipe(gulp.dest(destination + '/' + asset.name))));
+    const src = (asset) => gulp.src(asset.path);
+    return all(assets.map((asset) => (beforeDest ? beforeDest(src(asset), asset) : src(asset))
+        .pipe(gulp.dest(destination + '/' + asset.name))));
 };
 
 module.exports = copyAssets;
