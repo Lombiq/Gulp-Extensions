09da0fbaf8bb6c8776a8cc3726a803282cda7b45
Márk Lukovics <mark.lukovics@lombiq.com>
2021-02-16 14:47:51 +0100

    Changing " to '
    adding rule 'function-paren-newline'  for 3 or more items

--- ESLint/lombiq-base.js
+++ ESLint/lombiq-base.js
@@ -1,161 +1,171 @@
 /* eslint-disable */
 module.exports = {
-    "extends": "./eslint-base.js",
+    'extends': './eslint-base.js',
 
     // Here we can define our own rules overriding the base rules
-    "rules": {
+    'rules': {
         // Lombiq rules
-        "max-len": [
-            "error",
+        'max-len': [
+            'error',
             120,
             2,
             {
-                "ignoreUrls": true,
-                "ignoreComments": false,
-                "ignoreRegExpLiterals": true,
-                "ignoreStrings": true,
-                "ignoreTemplateLiterals": true
+                'ignoreUrls': true,
+                'ignoreComments': false,
+                'ignoreRegExpLiterals': true,
+                'ignoreStrings': true,
+                'ignoreTemplateLiterals': true
             }
         ],
 
-        "brace-style": [
-            "error",
-            "stroustrup",
-            { "allowSingleLine": true }
+        'brace-style': [
+            'error',
+            'stroustrup',
+            { 'allowSingleLine': true }
         ],
 
-        "prefer-template": "off",
+        'prefer-template': 'off',
 
-        "no-plusplus": [
-            "error",
-            { "allowForLoopAfterthoughts": true }
+        'no-plusplus': [
+            'error',
+            { 'allowForLoopAfterthoughts': true }
         ],
 
-        "linebreak-style": ["error", "windows"],
+        'linebreak-style': ['error', 'windows'],
 
-        "no-param-reassign": [
-            "error",
+        'no-param-reassign': [
+            'error',
             {
-                "props": false
+                'props': false
             }
         ],
 
-        "wrap-iife": [
-            "error",
-            "any", // outside originally
-            { "functionPrototypeMethods": false }
+        'wrap-iife': [
+            'error',
+            'any', // outside originally
+            { 'functionPrototypeMethods': false }
         ],
 
-        "prefer-arrow-callback": [
-            "error",
+        'prefer-arrow-callback': [
+            'error',
             {
-                "allowNamedFunctions": true, // false originally
-                "allowUnboundThis": true
+                'allowNamedFunctions': true, // false originally
+                'allowUnboundThis': true
             }
         ],
 
-        "no-underscore-dangle": [
-            "error",
+        'no-underscore-dangle': [
+            'error',
             {
-                "allowAfterThis": true
+                'allowAfterThis': true
             }
         ],
 
-        "no-restricted-syntax": [
-            "error",
+        'no-restricted-syntax': [
+            'error',
             {
-                "selector": "ForOfStatement",
-                "message": "iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations."
+                'selector': 'ForOfStatement',
+                'message': 'iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations.'
             },
             {
-                "selector": "LabeledStatement",
-                "message": "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand."
+                'selector': 'LabeledStatement',
+                'message': 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.'
             },
             {
-                "selector": "WithStatement",
-                "message": "`with` is disallowed in strict mode because it makes code impossible to predict and optimize."
+                'selector': 'WithStatement',
+                'message': '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
             }
         ],
 
-        "no-unused-expressions": [
-            "error",
+        'no-unused-expressions': [
+            'error',
             {
-                "allowShortCircuit": true,
-                "allowTernary": false,
-                "allowTaggedTemplates": false
+                'allowShortCircuit': true,
+                'allowTernary': false,
+                'allowTaggedTemplates': false
             }
         ],
 
-        "operator-linebreak": [
-            "error",
-            "after",
+        'operator-linebreak': [
+            'error',
+            'after',
             {
-                "overrides": {
-                    "=": "none",
-                    "?": "ignore",
-                    ":": "ignore"
+                'overrides': {
+                    '=': 'none',
+                    '?': 'ignore',
+                    ':': 'ignore'
                 }
             }
         ],
 
-        "no-else-return": [
-            "error",
-            { "allowElseIf": true }
+        'no-else-return': [
+            'error',
+            { 'allowElseIf': true }
         ],
 
-        "object-shorthand": [
-            "error",
-            "consistent-as-needed"
+        'object-shorthand': [
+            'error',
+            'consistent-as-needed'
         ],
 
-        "prefer-destructuring": [
-            "error",
+        'prefer-destructuring': [
+            'error',
             {
-                "VariableDeclarator": {
-                    "array": false,
-                    "object": false
+                'VariableDeclarator': {
+                    'array': false,
+                    'object': false
                 },
-                "AssignmentExpression": {
-                    "array": true,
-                    "object": false
+                'AssignmentExpression': {
+                    'array': true,
+                    'object': false
                 }
             },
             {
-                "enforceForRenamedProperties": false
+                'enforceForRenamedProperties': false
             }
         ],
 
-        "indent": [
-            "error",
+        'indent': [
+            'error',
             4,
             {
-                "SwitchCase": 1,
-                "VariableDeclarator": 1,
-                "outerIIFEBody": 1,
+                'SwitchCase': 1,
+                'VariableDeclarator': 1,
+                'outerIIFEBody': 1,
                 // MemberExpression: null,
-                "FunctionDeclaration": {
-                    "parameters": 1,
-                    "body": 1
+                'FunctionDeclaration': {
+                    'parameters': 1,
+                    'body': 1
                 },
-                "FunctionExpression": {
-                    "parameters": 1,
-                    "body": 1
+                'FunctionExpression': {
+                    'parameters': 1,
+                    'body': 1
                 },
-                "CallExpression": {
-                    "arguments": 1
+                'CallExpression': {
+                    'arguments': 1
                 },
-                "ArrayExpression": 1,
-                "ObjectExpression": 1,
-                "ImportDeclaration": 1,
-                "flatTernaryExpressions": false,
+                'ArrayExpression': 1,
+                'ObjectExpression': 1,
+                'ImportDeclaration': 1,
+                'flatTernaryExpressions': false,
                 // list derived from https://github.com/benjamn/ast-types/blob/HEAD/def/jsx.js
-                "ignoredNodes": ["JSXElement", "JSXElement > *", "JSXAttribute", "JSXIdentifier", "JSXNamespacedName", "JSXMemberExpression", "JSXSpreadAttribute", "JSXExpressionContainer", "JSXOpeningElement", "JSXClosingElement", "JSXFragment", "JSXOpeningFragment", "JSXClosingFragment", "JSXText", "JSXEmptyExpression", "JSXSpreadChild"],
-                "ignoreComments": false
+                'ignoredNodes': ['JSXElement', 'JSXElement > *', 'JSXAttribute', 'JSXIdentifier', 'JSXNamespacedName', 'JSXMemberExpression', 'JSXSpreadAttribute', 'JSXExpressionContainer', 'JSXOpeningElement', 'JSXClosingElement', 'JSXFragment', 'JSXOpeningFragment', 'JSXClosingFragment', 'JSXText', 'JSXEmptyExpression', 'JSXSpreadChild'],
+                'ignoreComments': false
             }
         ],
 
-        "func-names": ["error", "as-needed"],
+        'func-names': ['error', 'as-needed'],
 
-        "no-alert": "off"
+        'no-alert': 'off',
+
+        'function-paren-newline': ['error', { 'minItems': 3 }],
+
+        'comma-dangle': ['error', {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'never',
+        }],
     }
 }
