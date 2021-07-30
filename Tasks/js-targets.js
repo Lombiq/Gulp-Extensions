50f08e8c04e072d5e9c219e8775afca80cec91dd
Oliver Friedrich <oliver.friedrich@lombiq.com>
2021-07-30 10:55:43 +0200

    Add new compileFile method to avoid problems when trying to distinguish a single file from a folder or a glob pattern

--- Tasks/js-targets.js
+++ Tasks/js-targets.js
@@ -4,8 +4,20 @@ const eslint = require('gulp-eslint');
 
 function compile(source, destination, pipelineModifier) {
     const copyDestination = destination || source;
+    const sourceStream = gulp.src(source + '**/*.js')
 
-    let pipeline = gulp.src(source + '**/*.js')
+    return compileStream(sourceStream, copyDestination, pipelineModifier);
+}
+
+function compileFile(sourceFile, destination, pipelineModifier) {
+    const copyDestination = destination || sourceFile;
+    const sourceStream = gulp.src(sourceFile)
+
+    return compileStream(sourceStream, copyDestination, pipelineModifier);
+}
+
+function compileStream(sourceStream, destination, pipelineModifier) {
+    let pipeline = sourceStream
         .pipe(eslint())
         .pipe(eslint.format('tap'));
 
@@ -13,11 +25,11 @@ function compile(source, destination, pipelineModifier) {
         pipeline = pipelineModifier(pipeline);
     }
 
-    return pipeline.pipe(gulp.dest(copyDestination));
+    return pipeline.pipe(gulp.dest(destination));
 }
 
 function clean(destination) {
     return () => del(destination + '**/*.js');
 }
 
-module.exports = { compile, clean };
+module.exports = { compile, compileFile, clean };
