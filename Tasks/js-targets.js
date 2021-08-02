872fce617cd643f788e65f7fbc1a5cc7255543a1
Oliver Friedrich <oliver.friedrich@lombiq.com>
2021-08-03 00:28:54 +0200

    Improve compile method to accept source stream, add docs

--- Tasks/js-targets.js
+++ Tasks/js-targets.js
@@ -2,21 +2,31 @@ const gulp = require('gulp');
 const del = require('del');
 const eslint = require('gulp-eslint');
 
+/**
+ * This callback type is called `pipelineModifier` and is displayed as a global symbol.
+ *
+ * @callback pipelineModifier
+ * @param {NodeJS.ReadWriteStream} pipeline The files pipeline.
+ * @returns {NodeJS.ReadWriteStream} The files pipeline.
+ */
+
+/**
+ * This method processes all JavaScript files in the `source` folder incl. all subfolders, and saves the resulting
+ * files in the `destination` folder.
+ * 
+ * @param {string | NodeJS.ReadWriteStream} source The source folder path, or a `gulp.src()` object.
+ * @param {string} destination The destination folder path; if null, `source` will be used.
+ * @param {pipelineModifier} pipelineModifier A callback to be executed on the source files before saving them to disk.
+ * @returns {NodeJS.ReadWriteStream} The files pipeline.
+ */
 function compile(source, destination, pipelineModifier) {
+    if (typeof source !== 'string' && !destination) {
+        throw 'Please provide a "destination" folder for the processed files!';
+    }
+
     const copyDestination = destination || source;
-    const sourceStream = gulp.src(source + '**/*.js')
+    const sourceStream = typeof source === 'string' ? gulp.src(source + '**/*.js') : source;
 
-    return compileStream(sourceStream, copyDestination, pipelineModifier);
-}
-
-function compileFile(sourceFile, destination, pipelineModifier) {
-    const copyDestination = destination || sourceFile;
-    const sourceStream = gulp.src(sourceFile)
-
-    return compileStream(sourceStream, copyDestination, pipelineModifier);
-}
-
-function compileStream(sourceStream, destination, pipelineModifier) {
     let pipeline = sourceStream
         .pipe(eslint())
         .pipe(eslint.format('tap'));
@@ -25,11 +35,11 @@ function compileStream(sourceStream, destination, pipelineModifier) {
         pipeline = pipelineModifier(pipeline);
     }
 
-    return pipeline.pipe(gulp.dest(destination));
+    return pipeline.pipe(gulp.dest(copyDestination));
 }
 
 function clean(destination) {
     return () => del(destination + '**/*.js');
 }
 
-module.exports = { compile, compileFile, clean };
+module.exports = { compile, clean };
