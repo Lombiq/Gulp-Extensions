364ee49ee17144ec9454195e23ea7f0db7a5293f
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-08-05 14:37:47 +0200

    Fixing ESLint warnings in the browsersync task

--- Tasks/browsersync.js
+++ Tasks/browsersync.js
@@ -3,14 +3,14 @@ const browsersync = require('browser-sync').create('Browsersync Server');
 async function start(options) {
     const defaultOptions = {
         open: false,
-        online: false
-    }
+        online: false,
+    };
 
-    // Merge object properties with the spread operator. 
+    // Merge object properties with the spread operator.
     // In the case of a key collision, the right-most (last) object's value wins out.
     browsersync.init({ ...defaultOptions, ...options });
 
-    browsersync.watch(options.files, function (event, file) {
+    browsersync.watch(options.files, (event, file) => {
         if (event === 'change') {
             browsersync.notify(`${file} has been changed.`, 2000);
         }
